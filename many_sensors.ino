834a2352c57fae7ac46e5645b0b04b481ae4accb
Joe Delfino <jdelfino@gmail.com>
2023-05-11 09:40:53 -0400

    Update many_sensors.ino
    Cleanup & documentationf

--- many_sensors.ino
+++ many_sensors.ino
@@ -1,40 +1,60 @@
+/*
+ * This file contains an example of code that controls the blinking of an LED based on
+ * several input sensors.
+ * 
+ * This project requires the HCSR04 library by Martin Sosic https://github.com/Martinsos/arduino-lib-hc-sr04. 
+ * Install it through Tools->Manage Libraries. (Make sure you get the library by Martin Sosic, there are many
+ * HC-SR04 libraries available)
+ * 
+ * The wiring for this project is as follows:
+ * - 1 LED wired to pin #12
+ * - 1 toggle switch wired to pin #11
+ * - 1 (knob) potentiometer wired to analog input #0 (A0)
+ * - 1 slide potentiometer wired to analog input #1 (A1)
+ * - 1 ultrasonic sensor (HC-SR04) with trigger wired to pin #10 and echo to pin #8
+ * 
+ * See https://github.com/Beaver-Country-Day-School/arduino_examples for simpler examples 
+ * that go more in-depth on each sensor type.
+ */
+
 #include <HCSR04.h>
 
 const int LED_PIN = 12;
-const int BUTTON_PIN = 11;
-//const int TWIST_POTENTIONMETER = 10;
+const int SWITCH_PIN = 11;
 
 const int TRIGGER_PIN = 10;
 const int ECHO_PIN = 8;
+
+const int KNOB_INPUT = A0; // analog pin 0
+const int SLIDE_INPUT = A1; // analog pin 1
+
 UltraSonicDistanceSensor distanceSensor(TRIGGER_PIN, ECHO_PIN);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  //pinMode(TWIST_POTENTIOMETER, INPUT);
 
   Serial.begin(9600); // open the serial port at 9600 bps:
 }
 
 void loop() {
 
-  const int buttonState = digitalRead(BUTTON_PIN);
-  int knobValue = analogRead(A0);
-  int slideValue = analogRead(A1);
+  const int buttonState = digitalRead(SWITCH_PIN);
+  int knobValue = analogRead(KNOB_INPUT);
+  int slideValue = analogRead(SLIDE_INPUT);
 
   float distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);  
 
+  Serial.print("Knob: ");
   Serial.print(knobValue);
-  Serial.print(", ");
+  Serial.print(", Slide: ");
   Serial.print(slideValue);
-  Serial.print(", ");
+  Serial.print(", Distance (cm): ");
   Serial.print(distance);
   Serial.println();
 
-  if(distance < 15.0 && distance > 0.0) {
+  if(distance > 0.0 && distance < 15.0) {
     digitalWrite(LED_PIN, HIGH);
     delay(50);
     digitalWrite(LED_PIN, LOW);
