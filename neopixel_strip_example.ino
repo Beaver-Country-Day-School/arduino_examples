2a8f03e343836d0270cf428cf2b8ba220e5cf30d
Joe Delfino <jdelfino@gmail.com>
2023-05-23 15:05:31 -0400

    Update neopixel_strip_example.ino
    simplify

--- neopixel_strip_example.ino
+++ neopixel_strip_example.ino
@@ -23,40 +23,36 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define PIN_NEO_PIXEL  10   // Arduino pin that connects to NeoPixel
-#define NUM_PIXELS     20  // The number of LEDs (pixels) on NeoPixel
+int PIN_NEO_PIXEL = 10;   // Arduino pin that connects to NeoPixel
+int NUM_PIXELS = 20;  // The number of LEDs (pixels) on NeoPixel
 
-Adafruit_NeoPixel NeoPixel(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    NeoPixel.begin(); // Initialize the neopixel strip - this is required
+    pixels.begin(); // Initialize the neopixel strip - this is required
 }
 
 void loop() {
-  NeoPixel.clear(); // set all pixel colors to 'off'. It only takes effect if pixels.show() is called
+  pixels.clear(); // set all pixel colors to 'off'. It only takes effect if pixels.show() is called
+  pixels.show();
 
   // turn pixels to green one by one with delay between each pixel
   for (int pixel = 0; pixel < NUM_PIXELS; pixel++) { // for each pixel
-    NeoPixel.setPixelColor(pixel, NeoPixel.Color(0, 255, 0)); // it only takes effect if pixels.show() is called
-    NeoPixel.show();   // send the updated pixel colors to the NeoPixel hardware.
+    pixels.setPixelColor(pixel, pixels.Color(0, 255, 0)); // it only takes effect if pixels.show() is called
+    pixels.show();   // send the updated pixel colors to the NeoPixel hardware.
     delay(200); // pause between each pixel
   }
   delay(2000);
 
-  // turn off all pixels for two seconds
-  NeoPixel.clear();
-  NeoPixel.show(); // send the updated pixel colors to the NeoPixel hardware.
-  delay(2000);     // off time
-
   // turn on all pixels to red at the same time for two seconds
   for (int pixel = 0; pixel < NUM_PIXELS; pixel++) { // for each pixel
-    NeoPixel.setPixelColor(pixel, NeoPixel.Color(255, 0, 0)); // it only takes effect if pixels.show() is called
+    pixels.setPixelColor(pixel, pixels.Color(255, 0, 0)); // it only takes effect if pixels.show() is called
   }
-  NeoPixel.show(); // send the updated pixel colors to the NeoPixel hardware.
+  pixels.show(); // send the updated pixel colors to the NeoPixel hardware.
   delay(2000);     // on time
 
   // turn off all pixels for one seconds
-  NeoPixel.clear();
-  NeoPixel.show(); // send the updated pixel colors to the NeoPixel hardware.
+  pixels.clear();
+  pixels.show(); // send the updated pixel colors to the NeoPixel hardware.
   delay(2000);     // off time  
 }
